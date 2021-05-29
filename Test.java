fd3a7e7ffe637bacedba82fc7fb632e23399e662
VinayagamD <vinayagam.d.ganesh@gmail.com>
2021-05-29 11:18:36 +0530

    Inital code to save

---
+++ Test.java
@@ -0,0 +1,9 @@
+public class Test{
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        for (String arg : args) {
+            System.out.println("Arg = " + arg);
+        }
+    }
+}
